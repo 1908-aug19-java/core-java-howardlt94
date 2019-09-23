ccee3d3b89c65c53a80c44b1eba66ff088a8b3b8
howardlt94 <45401260+howardlt94@users.noreply.github.com>
2019-09-23 17:37:43 -0400

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FirstComponent } from './components/first.component';
+import { DatabindingComponent } from './components/databinding/databinding.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    DatabindingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
